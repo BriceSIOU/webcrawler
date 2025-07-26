a5e4a0d343db30592dfe44077479aaaae8547e1b
Brice SIOU <eninambrices@gmail.com>
2025-07-26 18:09:24 +0100

    add of getURLsFromHTML function  and tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const{normalizeURL } = require('./crawl.js')
+const{normalizeURL,  getURLsFromHTML} = require('./crawl.js')
 const {test, expect} = require('@jest/globals')
 
 test('normalizeURL strip protocol https', ()=> {
@@ -26,5 +26,70 @@ test('normalizeURL strip protocol http', ()=> {
     const input = 'http://bricesiou.com/resources'
     const actual = normalizeURL(input)
     const expected = 'bricesiou.com/resources'
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML for the absolute', ()=> {
+    const inputHTMLBody =`
+    <html>
+     <body>
+       <a href="https://bricesiou.com/resources/">
+       Brice SIOU site
+       </a>
+    </html>
+    `
+    const inputBaseURL = "https://bricesiou.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://bricesiou.com/resources/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML for the relative', ()=> {
+    const inputHTMLBody =`
+    <html>
+     <body>
+       <a href="/resources/">
+       Brice SIOU site
+       </a>
+    </html>
+    `
+    const inputBaseURL = "https://bricesiou.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://bricesiou.com/resources/']
+    expect(actual).toEqual(expected)
+})
+
+test('getURLsFromHTML for the relative and absolute', ()=> {
+    const inputHTMLBody =`
+    <html>
+     <body>
+       <a href="/resources1/">
+       Brice SIOU site 1
+       </a>
+
+       <a href="https://bricesiou.com/resources2/">
+       Brice SIOU site 2
+       </a>
+    </html>
+    `
+    const inputBaseURL = "https://bricesiou.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = ['https://bricesiou.com/resources1/', 'https://bricesiou.com/resources2/']
+    expect(actual).toEqual(expected)
+})
+
+
+test('Invalid url test', ()=> {
+    const inputHTMLBody =`
+    <html>
+     <body>
+       <a href="Invalid">
+       Invalid SITE
+       </a>
+    </html>
+    `
+    const inputBaseURL = "https://bricesiou.com"
+    const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL)
+    const expected = []
     expect(actual).toEqual(expected)
 })
